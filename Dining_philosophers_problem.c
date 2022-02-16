c824f925e44893c625b63858f52595fa585ab3a6
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-02-16 14:20:04 +0100

    tra

--- Dining_philosophers_problem.c
+++ Dining_philosophers_problem.c
@@ -3,51 +3,49 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define __MAX_NUM__ 5
-
-pthread_mutex_t locks[__MAX_NUM__];
+pthread_mutex_t locks[5];
 
 typedef struct args{
     int id;
-    char *nome;
+    char *name;
 } args;
 
-void *mangia(void *arg){
+void *eat(void *arg){
     int id = ((args *) arg) -> id;
-    char *nome = ((args *) arg) -> nome;
+    char *name = ((args *) arg) -> name;
     int forks[2];
     int counter = 0;
 
     forks[0] = id;
     
     if(id == 0){
-        forks[1] = __MAX_NUM__ - 1;
+        forks[1] = 5 - 1;
     }else{
         forks[1] = id - 1;
     }
 
     while(1){
         pthread_mutex_lock(&locks[forks[0]]);
-        printf("i'm %s and i take the left fork\n", nome);
+        printf("i'm %s and i take the left fork\n", name);
         pthread_mutex_lock(&locks[forks[1]]);
-        printf("i'm %s and i take the right fork\n", nome);
+        printf("i'm %s and i take the right fork\n", name);
     
-        printf("i'm %s and i'm eating\n", nome);
+        printf("i'm %s and i'm eating\n", name);
         sleep(1);
         counter++;
-        printf("i'm %s and i'm done eating %d times\n", nome, counter);
+        printf("i'm %s and i'm done eating %d times\n", name, counter);
 
         pthread_mutex_unlock(&locks[forks[0]]);
-        printf("i'm %s and i leave the left fork\n", nome);
+        printf("i'm %s and i leave the left fork\n", name);
         pthread_mutex_unlock(&locks[forks[1]]);
-        printf("i'm %s and i leave the right fork\n", nome);
+        printf("i'm %s and i leave the right fork\n", name);
     }
 
     return NULL;     
 }
 
 int main(){
-    pthread_t filosofi[__MAX_NUM__];
+    pthread_t philosophers[5];
 
     args arg0 = {0, "0: alberto"};
     args arg1 = {1, "1: francesco"};
@@ -55,17 +53,17 @@ int main(){
     args arg3 = {3, "3: gianmarco"};
     args arg4 = {4, "4: steve"};
 
-    pthread_create(&filosofi[0], NULL, mangia, &arg0);
-    pthread_create(&filosofi[1], NULL, mangia, &arg1);
-    pthread_create(&filosofi[2], NULL, mangia, &arg2);
-    pthread_create(&filosofi[3], NULL, mangia, &arg3);
-    pthread_create(&filosofi[4], NULL, mangia, &arg4);
+    pthread_create(&philosophers[0], NULL, eat, &arg0);
+    pthread_create(&philosophers[1], NULL, eat, &arg1);
+    pthread_create(&philosophers[2], NULL, eat, &arg2);
+    pthread_create(&philosophers[3], NULL, eat, &arg3);
+    pthread_create(&philosophers[4], NULL, eat, &arg4);
 
-    pthread_join(filosofi[0], NULL);
-    pthread_join(filosofi[1], NULL);
-    pthread_join(filosofi[2], NULL);
-    pthread_join(filosofi[3], NULL);
-    pthread_join(filosofi[4], NULL);
+    pthread_join(philosophers[0], NULL);
+    pthread_join(philosophers[1], NULL);
+    pthread_join(philosophers[2], NULL);
+    pthread_join(philosophers[3], NULL);
+    pthread_join(philosophers[4], NULL);
 
     return 0;
 }
